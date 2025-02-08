4629b7ee11eaeacc8baa58a616b60aa38a8b993d
Ivan Espinoza <edgarivanespinoza@gmail.com>
2025-02-08 02:55:44 +0100

    Add Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,39 @@
 'use-client';
 
-import "./globals.css";
-import Providers from "./providers";
+import './globals.css';
+import Providers from './providers';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata = {
+  title: 'PixelHitos - Tu historia en mapas personalizados',
+  description:
+    'Regala y decora con tus emociones, lugares, sonidos... con hitos de tu historia.',
+  openGraph: {
+    type: 'website',
+    url: 'https://www.pixelhitos.com',
+    title: 'PixelHitos - Tu historia en mapas personalizados',
+    description:
+      'Regala y decora con tus emociones, lugares, sonidos... con hitos de tu historia.',
+    siteName: 'PixelHitos',
+    images: [
+      {
+        url: 'https://www.pixelhitos.com/assets/images/MAPA-Graffiti-01.png',
+        width: 1200,
+        height: 630,
+        alt: 'Mapa personalizado - PixelHitos',
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="es">
+    <html lang='es'>
       <body>
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
